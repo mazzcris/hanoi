f8f44d5f2a68185ff624edca383cb8ac0183d9f2
Cristiano Mazzarini <cristiano.mazzarini@gmail.com>
2024-11-07 12:17:28 +0100

    Add game win check

--- src/Game.php
+++ src/Game.php
@@ -1,6 +1,26 @@
 <?php
 
+namespace Hanoi;
+
 class Game
 {
+    private array $tower1 = [];
+    private array $tower2 = [];
+    private array $tower3 = [];
 
+    public function checkWin(): bool
+    {
+        if (!empty($this->tower1) || !empty($this->tower2)) {
+            return false;
+        }
+
+        return [1, 2, 3, 4, 5, 6, 7] === $this->tower3;
+    }
+
+    public function setState(array $tower1, array $tower2, array $tower3): void
+    {
+        $this->tower1 = $tower1;
+        $this->tower2 = $tower2;
+        $this->tower3 = $tower3;
+    }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -1,8 +1,35 @@
 <?php
 
-namespace tests;
+namespace Hanoi\tests;
 
-class GameTest
+use Hanoi\Game;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class GameTest extends TestCase
 {
+    #[Test]
+    #[DataProvider('stateProvider')]
+    public function testCheckWin(array $tower1, array $tower2, array $tower3, bool $isWin): void
+    {
+        $game = new Game();
+        $game->setState(
+            $tower1,
+            $tower2,
+            $tower3
+        );
+
+        $this->assertEquals($isWin, $game->checkWin());
+    }
+
+    static function stateProvider(): array
+    {
+        return [
+            [[], [], [1, 2, 3, 4, 5, 6, 7], true],
+            [[], [2, 3], [1, 4, 5, 6, 7], false],
+            [[1], [2], [3, 4, 5, 6, 7], false],
+        ];
+    }
 
 }
